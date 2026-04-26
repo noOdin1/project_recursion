e174a75253f86a5287b5462731d48e1659b6904a
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2026-04-27 00:16:49 +0800

    Test adapted from previous exercise, test for fibonacciGrp.fibs

--- fibonacci.test.js
+++ fibonacci.test.js
@@ -1,3 +1,39 @@
 // import { fibonacciGrp } from "./fibonacci";
 const fibonacciGrp = require("./fibonacci");
 
+describe("fibonacci using iterative method", () => {
+  test("4th fibonacci number is 3", () => {
+    expect(fibonacciGrp.fibs(4)).toBe(3);
+  });
+  test("7th fibonacci number is 13", () => {
+    expect(fibonacciGrp.fibs(7)).toBe(13);
+  });
+  test("10th fibonacci number is 55", () => {
+    expect(fibonacciGrp.fibs(10)).toBe(55);
+  });
+  test("15th fibonacci number is 610", () => {
+    expect(fibonacciGrp.fibs(15)).toBe(610);
+  });
+  test("25th fibonacci number is 75025", () => {
+    expect(fibonacciGrp.fibs(25)).toBe(75025);
+  });
+  test("0th fibonacci number is 0", () => {
+    expect(fibonacciGrp.fibs(0)).toBe(0);
+  });
+  test("doesn't accept negatives", () => {
+    expect(fibonacciGrp.fibs(-25)).toBe("OOPS");
+  });
+  test("DOES accept strings", () => {
+    expect(fibonacciGrp.fibs("0")).toBe(0);
+  });
+  test("DOES accept strings", () => {
+    expect(fibonacciGrp.fibs("1")).toBe(1);
+  });
+  test("DOES accept strings", () => {
+    expect(fibonacciGrp.fibs("2")).toBe(1);
+  });
+  test("DOES accept strings", () => {
+    expect(fibonacciGrp.fibs("8")).toBe(21);
+  });
+});
+
