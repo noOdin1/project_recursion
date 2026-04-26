bbeccb864110832622ccd1f1a270affde0c97355
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2026-04-27 00:19:33 +0800

    Coded fibs using recursive methods to get fibonacci term

--- fibonacci.js
+++ fibonacci.js
@@ -2,6 +2,15 @@
 /* fibonacci.js */
 
 const fibonacciGrp = {
+  fibsRec: (num) => {
+    if (num < 0) return "OOPS";
+    return num < 2
+      ? Number(num)
+      : Number(fibonacciGrp.fibsRec(num - 1) + fibonacciGrp.fibsRec(num - 2));
+  },
+
+  // console.log("The 8th term in the fibonacci series is " + fibsRec(8));
+
 };
 
 // export { fibonacciGrp };
