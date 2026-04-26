f46f2d5db5ec007fb006b651885fe8d3abdda26f
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2026-04-27 00:19:57 +0800

    Fibonacci term calculation using iterative method

--- fibonacci.js
+++ fibonacci.js
@@ -11,6 +11,17 @@ const fibonacciGrp = {
 
   // console.log("The 8th term in the fibonacci series is " + fibsRec(8));
 
+  fibs: (num) => {
+    if (num < 0) return "OOPS";
+    if (num == 0 || num == "0") return 0;
+    if (num == 2 || num == "2" || num == 1 || num == "1") return 1;
+    let fibArray = [0, 1];
+    for (let i = 2; i <= num; i++) {
+      fibArray.push(fibArray[i - 1] + fibArray[i - 2]);
+    }
+    fibArray.splice(0, 1); // removing the first term as fibonacci sequence starts with 1, 1, 2, 3, 5, 8...
+    return fibArray.pop();
+  },
 };
 
 // export { fibonacciGrp };
