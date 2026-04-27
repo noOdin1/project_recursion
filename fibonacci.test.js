fb7d264b487b3e2c17edb4f31921102b0349e339
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2026-04-27 20:03:36 +0800

    Tests for recursive fibonacci, using Z-combinator

--- fibonacci.test.js
+++ fibonacci.test.js
@@ -150,3 +150,26 @@ describe("fibonacci sequence using recursion, testing fibsRecAlt02", () => {
   });
 });
 
+describe("fibonacci sequence using recursion, testing fibsRecAlt03", () => {
+  test("fibonacci sequence to the 4th term is [0, 1, 1, 2]", () => {
+    expect(fibonacciGrp.fibsRecAlt03(4)).toEqual([0, 1, 1, 2]);
+  });
+  test("doesn't accept negatives", () => {
+    expect(fibonacciGrp.fibsRecAlt03(-25)).toBe(undefined);
+  });
+  test("0th fibonacci seqeunce is 0", () => {
+    expect(fibonacciGrp.fibsRecAlt03(0)).toEqual([]);
+  });
+  test("DOES accept strings, sequence to the first term is [ 0 ]", () => {
+    expect(fibonacciGrp.fibsRecAlt03("1")).toEqual([0]);
+  });
+  test("DOES accept strings,sequence to the 2nd term is [0, 1]", () => {
+    expect(fibonacciGrp.fibsRecAlt03("2")).toEqual([0, 1]);
+  });
+  test("DOES accept strings, sequence to the 8th term is [0, 1, 1, 2, 3, 5, 8, 13]", () => {
+    expect(fibonacciGrp.fibsRecAlt03("8")).toEqual([0, 1, 1, 2, 3, 5, 8, 13]);
+  });
+  test("Sequence to the 3rd term is [0, 1, 1]", () => {
+    expect(fibonacciGrp.fibsRecAlt03(3)).toEqual([0, 1, 1]);
+  });
+});
