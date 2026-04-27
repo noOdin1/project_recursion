095a9efd25e54d6d1752dd8a734f7c10ed866a8b
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2026-04-27 19:59:00 +0800

    Tests for fibonacci using fibsRec, recursive fibonacci function

--- fibonacci.test.js
+++ fibonacci.test.js
@@ -42,38 +42,39 @@ describe("fibonacci sequence using iterative method", () => {
   });
 });
 
-describe("fibonacci using recursion", () => {
+describe("fibonacci nth term using recursion", () => {
   test("4th fibonacci number is 3", () => {
-    expect(fibonacciGrp.fibsRec(4)).toBe(3);
+    expect(fibonacciGrp.fibonacciTerm(4)).toBe(3);
   });
   test("7th fibonacci number is 13", () => {
-    expect(fibonacciGrp.fibsRec(7)).toBe(13);
+    expect(fibonacciGrp.fibonacciTerm(7)).toBe(13);
   });
   test("10th fibonacci number is 55", () => {
-    expect(fibonacciGrp.fibsRec(10)).toBe(55);
+    expect(fibonacciGrp.fibonacciTerm(10)).toBe(55);
   });
   test("15th fibonacci number is 610", () => {
-    expect(fibonacciGrp.fibsRec(15)).toBe(610);
+    expect(fibonacciGrp.fibonacciTerm(15)).toBe(610);
   });
   test("25th fibonacci number is 75025", () => {
-    expect(fibonacciGrp.fibsRec(25)).toBe(75025);
+    expect(fibonacciGrp.fibonacciTerm(25)).toBe(75025);
   });
   test("0th fibonacci number is 0", () => {
-    expect(fibonacciGrp.fibsRec(0)).toBe(0);
+    expect(fibonacciGrp.fibonacciTerm(0)).toBe(0);
   });
   test("doesn't accept negatives", () => {
-    expect(fibonacciGrp.fibsRec(-25)).toBe("OOPS");
+    expect(fibonacciGrp.fibonacciTerm(-25)).toBe(undefined);
   });
   test("DOES accept strings", () => {
-    expect(fibonacciGrp.fibsRec("0")).toBe(0);
+    expect(fibonacciGrp.fibonacciTerm("0")).toBe(0);
   });
   test("DOES accept strings", () => {
-    expect(fibonacciGrp.fibsRec("1")).toBe(1);
+    expect(fibonacciGrp.fibonacciTerm("1")).toBe(1);
   });
   test("DOES accept strings", () => {
-    expect(fibonacciGrp.fibsRec("2")).toBe(1);
+    expect(fibonacciGrp.fibonacciTerm("2")).toBe(1);
   });
   test("DOES accept strings", () => {
-    expect(fibonacciGrp.fibsRec("8")).toBe(21);
+    expect(fibonacciGrp.fibonacciTerm("8")).toBe(21);
   });
 });
+
