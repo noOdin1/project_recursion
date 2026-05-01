db3b3b25d011847460ce8a7809eb504718429735
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2026-05-01 19:47:52 +0800

    Copied all the changes of fibonacci.js

--- fibonacci.cjs
+++ fibonacci.cjs
@@ -58,6 +58,7 @@ const fibonacciGrp = {
   // Using Z-combinator, w/o using an external 'const fib'
   fibsRecAlt03: (num) => {
     if (num < 0) return undefined;
+    // let tmpArr = [];
 
     // Array.from({ length: num }, (_, i) => i).forEach((ele) => {
     //   // tmpArr.push(Z((k) => (n) => (n <= 1 ? n : k(n - 1) + k(n - 2)))(ele));
@@ -67,18 +68,16 @@ const fibonacciGrp = {
     //     )(ele),
     //   );
     // });
-    //
     // return tmpArr;
 
     return Array.from({ length: num }, (_, i) => i).map((ele) => {
-      // tmpArr.push(Z((k) => (n) => (n <= 1 ? n : k(n - 1) + k(n - 2)))(ele));
       return ((g) => ((x) => g((v) => x(x)(v)))((x) => g((v) => x(x)(v))))(
         (k) => (n) => (n <= 1 ? n : k(n - 1) + k(n - 2)),
       )(ele);
     });
   },
 
-  // Using IIFE
+  // IIFE method
   fibsRecAlt04: (num) => {
     if (num < 0) return undefined;
 
@@ -95,13 +94,80 @@ const fibonacciGrp = {
     if (num < 0) return undefined;
     if (num == 0 || num == "0") return 0;
     if (num == 1 || num == "1") return [0];
-    let fibArray = [0, 1];
-    for (let i = 2; i <= num; i++) {
-      fibArray.push(fibArray[i - 1] + fibArray[i - 2]);
+
+    // Either of this code blocks will work
+    // let fibArray = [0, 1];
+    // for (let i = 2; i <= num - 1; i++) {
+    //   fibArray.push(fibArray[i - 1] + fibArray[i - 2]);
+    // }
+
+    let fibArray = [];
+    for (let i = 0; i <= num - 1; i++) {
+      i <= 1
+        ? fibArray.push(i)
+        : fibArray.push(fibArray[i - 1] + fibArray[i - 2]);
     }
-    // fibArray.splice(0, 1); // removing the first term as fibonacci sequence starts with 1, 1, 2, 3, 5, 8...
-    fibArray.pop();
+    // fibArray.splice(0, 1); // removing the first term if fibonacci sequence starts with 1, 1, 2, 3, 5, 8...
     return fibArray;
+  },
+
+  /* Fibonacci using Iterative method variation 1 */
+  fibsIterative01: (num) => {
+    if (num < 0) return undefined;
+    if (num == 0 || num == "0") return 0;
+    if (num == 1 || num == "1") return [0];
+
+    /* Memory storage for the Array.map anonymous function */
+    let minusOne = 1;
+    let minusTwo = 0;
+    return Array.from({ length: num }, (_, i) => {
+      if (i <= 1) {
+        minusTwo = minusOne;
+        minusOne = i;
+        return i;
+      }
+      let tmpVal = minusOne + minusTwo;
+      minusTwo = minusOne;
+      minusOne = tmpVal;
+      return tmpVal;
+    });
+  },
+
+  /* Fibonacci using Iterative method variation 2 */
+  fibsIterative02: (num) => {
+    if (num < 0) return undefined;
+    if (num == 0 || num == "0") return 0;
+    if (num == 1 || num == "1") return [0];
+
+    let fibArray = []; /* Memory for the Array.map function call */
+    return Array.from({ length: num }, (_, i) => {
+      if (i <= 1) {
+        fibArray.push(i);
+        return i;
+      }
+      fibArray.push(fibArray[i - 1] + fibArray[i - 2]);
+      return fibArray[fibArray.length - 1];
+    });
+  },
+
+  fibsIterative03: (num) => {
+    if (num < 0) return undefined;
+    if (num == 0 || num == "0") return 0;
+    if (num == 1 || num == "1") return [0];
+
+    /* Memory storage for the Array.map anonymous function */
+    let minusOne = 1;
+    let minusTwo = 0;
+    return Array.from({ length: num }, (_, i) => {
+      if (i <= 1) {
+        // Array destructuring notation
+        [minusOne, minusTwo] = [i, minusOne];
+        return i;
+      }
+      // Use Javascript's modern Array destructuring notation
+      [minusOne, minusTwo] = [minusOne + minusTwo, minusOne];
+      return minusOne;
+    });
   },
 };
 
